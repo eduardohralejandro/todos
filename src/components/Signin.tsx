ceef38c0a45c6782e839f8d308ea48587cc121d9
Eduardo Hernandez <eduardo@chatbot-plus.com>
2021-09-25 14:46:37 +0200

    connect react router

---
+++ src/components/Signin.tsx
@@ -0,0 +1,15 @@
+const Signin = () => {
+  return (
+    <div>
+      Signin
+      <form >
+        <input type='email' /> 
+        <input type="name" />
+        <button>sign in</button>
+      </form>
+    </div>
+  )
+}
+
+
+export default Signin;
